1040f2159a1668d30a35e6f56d191080a4fe64d5
Raza Tayyab <raza.tayyab@vizteck.com>
2016-03-21 18:22:52 +0500

    first day of node

---
+++ Gettingstarted.js
@@ -0,0 +1,31 @@
+var maxTimeTowait = 520;
+
+
+var evenDoubler = function(v,callback)
+{
+    var waitTime = Math.floor(Math.random()*maxTimeTowait);
+    if(v%2)
+    {
+        callback(NewError(" Odd Input"));
+    }
+    else
+    {
+        callback(null,v,v*2,waitTime);
+
+    }
+
+}
+var resultHandler = function(err,v,result,waitTime)
+{
+    if(err)
+    {
+        console.log(err);
+    }
+    else{
+        console.log("Result we get by doubling " + v +" in " + waitTime + "(ms) is " + result );
+
+    }
+}
+
+evenDoubler(2,resultHandler);
+
